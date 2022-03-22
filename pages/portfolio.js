5f601bf031aefa67f876319685542c97c8bc38bc
Brendan Stander <info@bren-10.co.za>
2022-03-22 17:10:43 +0200

    Improve splash page, start work on portfolio page

---
+++ pages/portfolio.js
@@ -0,0 +1,11 @@
+import { Head } from "next/document";
+import styles from "../styles/Portfolio.module.css";
+
+export default function portfolio() {
+  return (
+    <div className={styles.portfolio}>
+      <h1>Portfolio</h1>
+      <p>asdfasdfasdfasdfadfsadfasfasdf</p>
+    </div>
+  );
+}
