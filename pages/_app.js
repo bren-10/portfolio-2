7d596e684af2275af03f179e061e753d4ff39efa
Brendan Stander <info@bren-10.co.za>
2022-04-03 17:56:29 +0200

    Attempt background fix for vercel and change font importing

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "react-image-gallery/styles/css/image-gallery.css"
-import '../styles/globals.css'
-import Script from 'next/script';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-image-gallery/styles/css/image-gallery.css";
+import "../styles/globals.css";
+import Script from "next/script";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <Component {...pageProps} />
       <Script
         src="https://unpkg.com/react/umd/react.production.min.js"
@@ -22,7 +31,7 @@ function MyApp({ Component, pageProps }) {
         crossOrigin="true"
       ></Script>
     </div>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
